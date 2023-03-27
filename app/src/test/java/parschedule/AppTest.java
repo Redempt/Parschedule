3e3ffc65e0d9eda66ea95562225efa7d013f6193
Redempt <instantiable@gmail.com>
2023-03-27 13:36:05 -0400

    Create second unit test

--- app/src/test/java/parschedule/AppTest.java
+++ app/src/test/java/parschedule/AppTest.java
@@ -6,14 +6,27 @@ package parschedule;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    public static Duration diff(Calendar first, Calendar second) {
+        return Duration.ofMillis(second.getTimeInMillis() - first.getTimeInMillis());
+    }
+
     @Test
     public void simpleTimeOffsetTest() {
         assertEquals(TimeOffsetParser.parseTimeOffset("10s"), Duration.ofSeconds(10));
         assertEquals(TimeOffsetParser.parseTimeOffset("5m10s"), Duration.ofSeconds(310));
         assertEquals(TimeOffsetParser.parseTimeOffset("1d"), Duration.ofDays(1));
+    }
+
+    @Test
+    public void simpleTimeSetTest() {
+        Calendar threePM = Calendar.getInstance();
+        threePM.set(Calendar.HOUR_OF_DAY, 15);
+        assertEquals(TimeOffsetParser.parseTimeOffset("3:00pm"), diff(Calendar.getInstance(), threePM));
     }
 }
