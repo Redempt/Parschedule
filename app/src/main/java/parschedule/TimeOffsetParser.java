436fca9895136d6e6c0f6f63e3f5b4ae352f4a81
justinwillis145 <72421206+justinwillis145@users.noreply.github.com>
2023-03-29 17:23:45 -0400

    Update TimeOffsetParser.java

--- app/src/main/java/parschedule/TimeOffsetParser.java
+++ app/src/main/java/parschedule/TimeOffsetParser.java
@@ -8,9 +8,11 @@ import java.util.regex.Pattern;
 
 public class TimeOffsetParser {
 
-    public static Duration parseTimeOffset(String input) {
+    public static Duration parseTimeOffset(String input, String timeString) {
         Pattern pattern = Pattern.compile("(\\d+)([smhd])");
         Matcher matcher = pattern.matcher(input);
+        LocalTime time = LocalTime.parse(timeString.toUpperCase(), DateTimeFormatter.ofPattern("h:mma"));
+        return Duration.between(LocalTime.NOON, time);
 
         long seconds = 0;
         while (matcher.find()) {
@@ -37,11 +39,8 @@ public class TimeOffsetParser {
 
         return Duration.ofSeconds(seconds);
     }
-
-    public static Duration diff(Calendar from, Calendar to) {
-        LocalTime fromTime = LocalTime.of(from.get(Calendar.HOUR_OF_DAY), from.get(Calendar.MINUTE));
-        LocalTime toTime = LocalTime.of(to.get(Calendar.HOUR_OF_DAY), to.get(Calendar.MINUTE));
-        return Duration.between(fromTime, toTime);
+    public static Duration diff(Calendar cal1, Calendar cal2) {
+        return Duration.between(cal1.toInstant(), cal2.toInstant());
     }
 
     public static void main(String[] args) {
