14b3210ec0847b9b93edf3007a6fd5dba8a7e23b
justinwillis145 <72421206+justinwillis145@users.noreply.github.com>
2023-04-02 15:51:56 -0400

    Update Task.java

--- app/src/main/java/parschedule/Task.java
+++ app/src/main/java/parschedule/Task.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -20,52 +21,10 @@ public class Task {
     }
 
     public static Function<Task, String> serialize = task -> {
-        return task.getName() + "|" + task.getDescription();
+        return null;
     };
 
     public static Function<String, Task> deserialize = str -> {
-        String[] parts = str.split("\\|");
-        return new Task(parts[0], parts[1]);
+        return null;
     };
-
-    public static void main(String[] args) {
-        Scheduler scheduler = new Scheduler();
-        scheduler.addTask(new Task("Task 1", "Do something"));
-        scheduler.addTask(new Task("Task 2", "Do something else"));
-
-        List<String> taskStrings = scheduler.getTaskStrings();
-        System.out.println("Serialized tasks:");
-        taskStrings.forEach(System.out::println);
-
-        List<Task> tasks = scheduler.getTasks();
-        System.out.println("Deserialized tasks:");
-        tasks.forEach(task -> System.out.println(task.getName() + " - " + task.getDescription()));
-    }
-}
-
-class Scheduler {
-    private List<Task> tasks = new ArrayList<>();
-
-    public void addTask(Task task) {
-        tasks.add(task);
-    }
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public List<String> getTaskStrings() {
-        List<String> taskStrings = new ArrayList<>();
-        for (Task task : tasks) {
-            taskStrings.add(Task.serialize.apply(task));
-        }
-        return taskStrings;
-    }
-
-    public void setTaskStrings(List<String> taskStrings) {
-        tasks.clear();
-        for (String taskString : taskStrings) {
-            tasks.add(Task.deserialize.apply(taskString));
-        }
-    }
 }
